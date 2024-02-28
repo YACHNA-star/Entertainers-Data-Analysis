a87512ad654935b68d8180f1b030d6b6163665d5
YACHNA-star <73123152+YACHNA-star@users.noreply.github.com>
2024-02-28 22:36:18 +0530

    Add files via upload

--- SQLQuery.sql
+++ SQLQuery.sql
@@ -7,7 +7,6 @@ FROM
    Entertainer_final_data 
 ORDER BY 
     Total_Awards_won desc;
-
 --Q2. Most emmy awards win by which enterainer
 
 select top 5 Entertainer,Emmy_Won
@@ -16,28 +15,19 @@ ORDER BY Emmy_Won desc;
 
 --Q3. Most grammy awards win by which enterainer
 
- select top 5 Entertainer,Emmy_Won
+ select top 5 Entertainer,Grammy_Won
  from Entertainer_final_data
 ORDER BY Grammy_Won desc;
 
---Q4. Most grammy awards win by which enterainer
-
- select top 5 Entertainer,Emmy_Won
+--Q4. Most OSCAR awards win by which enterainer
+ select top 5 Entertainer,Oscar_Won
  from Entertainer_final_data
 ORDER BY Oscar_Won desc;
 
 --Q5. Most awards win by which gender
-
-SELECT 
-    Gender_traditional, 
+SELECT Gender_traditional, 
 COUNT(*) AS Total_Awards_won
 FROM Entertainer_final_data
 GROUP BY Gender_traditional
 ORDER BY Total_Awards_won DESC
 
---result
-/*
-Gender_traditional	
-M	50
-F	20
-*/
